7016a32f3353ea1bbc7e259a7584707764a386e9
tu7bxfga8 <tu7bxfga8@213-5>
2022-06-03 17:34:22 +0400

    Add project files.

---
+++ nestone/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using nestone.DAL;
+using nestone.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace nestone.Controllers
+{
+    public class HomeController : Controller
+    {
+        private AppDbContext _context { get; }
+       
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Slider> sliders = await _context.Sliders.ToListAsync()
+            return View(sliders)
+        }
+      
+
+    }
+}
